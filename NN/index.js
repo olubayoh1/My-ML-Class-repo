cc6d46b34cc4b7961f91b29a28186e100edf293d
olubayoh1 <130300254+olubayoh1@users.noreply.github.com>
2024-04-25 13:51:26 -0500

    Update index.js

--- NN/index.js
+++ NN/index.js
@@ -15,6 +15,25 @@ async function runExample() {
      x[8] = document.getElementById('box9').value;
      x[9] = document.getElementById('box10').value;
      x[10] = document.getElementById('box11').value;
+     x[11] = document.getElementById('box12').value;
+     x[12] = document.getElementById('box13').value;
+     x[13] = document.getElementById('box14').value;
+     x[14] = document.getElementById('box15').value;
+     x[15] = document.getElementById('box16').value;
+     x[16] = document.getElementById('box17').value;
+     x[17] = document.getElementById('box18').value;
+     x[18] = document.getElementById('box19').value;
+     x[19] = document.getElementById('box20').value;
+     x[20] = document.getElementById('box21').value;
+     x[21] = document.getElementById('box22').value;
+     x[22] = document.getElementById('box23').value;
+     x[23] = document.getElementById('box24').value;
+     x[24] = document.getElementById('box25').value;
+     x[25] = document.getElementById('box26').value;
+     x[26] = document.getElementById('box27').value;
+     x[27] = document.getElementById('box28').value;
+     x[28] = document.getElementById('box29').value;
+     x[29] = document.getElementById('box30').value;
 
     let tensorX = new onnx.Tensor(x, 'float32', [1, 30]);
 
